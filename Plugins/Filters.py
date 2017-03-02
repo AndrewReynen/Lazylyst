c786a036234f6a53dd87711edf34c5a3124f2ef4
AndrewReynen <andrew.m.g.reynen@gmail.com>
2017-03-02 17:19:33 -0500

    Optional to visibly split traces on a per-station basis

--- Plugins/Filters.py
+++ Plugins/Filters.py
@@ -1,5 +1,12 @@
+import numpy as np
 
 def streamFilter(*args,**kwargs):
+    # If the returned trace is to have all channels seperated on the y-axis...
+    # ...note for later
+    if 'sepChas' in kwargs.keys():
+        doSepChas=kwargs['sepChas']
+        kwargs.pop('sepChas')
+    # If want to return the raw, or derivative...
     stream=args[0]
     if kwargs['type']=='raw':
         return stream
@@ -11,5 +18,25 @@ def streamFilter(*args,**kwargs):
     if kwargs['type']=='derivative':
         stream.differentiate()
     else:
+        # Process the remaining arguments as obspy filter would
         stream.filter(**kwargs)
+    # If the channels were to be seperated, do after filtered
+    if doSepChas:
+        stream=sepChas(stream)
     return stream
+    
+# Shift traces up/down on a per-station basis so that they do not overlap
+def sepChas(stream):
+    # First sort the stream by channel name
+    stream.sort(keys=['channel'])
+    stas=np.array([tr.stats.station for tr in stream])
+    for sta in np.unique(stas):
+        args=np.where(stas==sta)[0]
+        offset=0
+        for arg in args:
+            aMin,aMax=np.min(stream[arg].data),np.max(stream[arg].data)
+            stream[arg].data-=(aMin-offset)
+            offset+=aMax-aMin
+    return stream
+        
+    
