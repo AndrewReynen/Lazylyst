592399bec52009ac8ce1bc188078e2f145f3949a
AndrewReynen <andrew.m.g.reynen@gmail.com>
2017-04-28 13:52:16 -0400

    change folder structure

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'lazylyst',
+  packages = ['lazylyst'], # this must be the same as the name above
+  version = '0.4.2',
+  description = 'GUI for timeseries review with a focus on seismology',
+  author = 'Andrew Reynen',
+  author_email = 'andrew.m.g.reynen@gmail.com',
+  url = 'https://github.com/AndrewReynen/Lazylyst', # use the URL to the github repo
+  download_url = 'https://github.com/AndrewReynen/Lazylyst/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['seismology', 'timeseries', 'picking', 'pyqtgraph'], # arbitrary keywords
+  classifiers = [],
+)
